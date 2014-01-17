62aaf613c195cbc4913f9bd89b9ca164d2da33a2
wowhy <hongy19890331@outlook.com>
2014-01-17 17:35:58 +0800

    美化界面

--- WxChatClient/MainWindow.xaml.cs
+++ WxChatClient/MainWindow.xaml.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WxChatClient.Common;
-
-namespace WxChatClient
+﻿namespace WxChatClient
 {
+    using FirstFloor.ModernUI.Windows.Controls;
+    using System.Windows;
+    using WxChatClient.Common;
+
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ModernWindow
     {
         public MainWindowModel ViewModel { get; set; }
 
